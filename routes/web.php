15e0e93b29b0727491eac2f803a758ad3bc2b253
Jefta Supraja <jefta.supraja@gmail.com>
2025-09-22 16:02:21 +0700

    feat: slicing detail movie

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,9 @@ Route::prefix('prototype')->name('prototype.')->group(function () {
     Route::get('/subscription', function (){
         return Inertia::render('Prototype/SubscriptionPlan');
     })->name('subscription');
+    Route::get('/movie/{slug}', function (){
+        return Inertia::render('Prototype/Movie/Show');
+    })->name('movie.show');
     
 });
 
